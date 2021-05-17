2f9d51d8220d6d67f69e5bc335aedc2debfc1b4b
Santosh Mondal <santosh.ece06@gmail.com>
2021-05-17 10:34:43 +0530

    exception handling.

---
+++ src/main/java/com/capgemini/exception/ErrorDetails.java
@@ -0,0 +1,13 @@
+package com.capgemini.exception;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+
+@Data
+public class ErrorDetails {
+
+	private String messsage;
+	private LocalDateTime timestamp;
+}
